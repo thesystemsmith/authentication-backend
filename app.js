7b98e80daa1556f08fe7d60ab5c5bfd01004cf11
abdul.rasheed <abdulrasheed.techy@gmail.com>
2024-06-06 15:29:27 +0530

    feat - user model and user registration logic with requried npm packages

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 
-//import db config
+//import files
 const dbConnect = require('./db/dbConnect')
+const User = require('./db/userModel')
 
 //execute connection
 dbConnect()
+
+// body parser configuration
+// need this to read the request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//register user
+app.post('/register', (req, res) => {
+    //hashing the password
+    bcrypt.hash(req.body.password, 10)
+        .then(hashedPassword => {
+            //create user
+            const user = new User({
+                email: req.body.email,
+                password: hashedPassword
+            })
+            //save user in db
+            user.save()
+                .then(result => {
+                    res.status(201).send({
+                        message: "User Created Successfully",
+                        result
+                    })
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: "Error creating user",
+                        err,
+                    })
+                })
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Password was not hashed successfully",
+                err,
+            })
+        })
+})
 
 module.exports = app;
