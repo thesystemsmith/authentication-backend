04fedadf4ce1e09b4374fa90bec921c691722090
abdul.rasheed <abdulrasheed.techy@gmail.com>
2024-06-06 16:05:35 +0530

    cors error handling

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ const auth = require('./auth')
 //execute connection
 dbConnect()
 
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    )
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    )
+    next()
+})
+
+
 // body parser configuration
 // need this to read the request body
 app.use(bodyParser.json());
